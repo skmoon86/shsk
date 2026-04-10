06b5b30f17fa8ba350d806fc993e2d3af7e2f7d7
mbs-skmoon <skmoon@mbsolution.co.kr>
2026-04-10 17:56:15 +0900

    chore: 빌드 태그 토스트 (삼성 인터넷 캐시 진단)
    앱 시작 시 빌드 식별자 + navigator.locks patched 여부를
    토스트로 노출. 구 버전 JS가 캐시되어 있으면 토스트가
    뜨지 않거나 다른 태그가 보인다.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import toast from 'react-hot-toast'
+
+// 삼성 인터넷 디버깅: 새 번들이 실제 로드되었는지 확인용
+const BUILD_TAG = 'b38c625-locks'
+setTimeout(() => { toast(`BUILD ${BUILD_TAG} / locks=${!!navigator.locks}`, { duration: 4000 }) }, 500)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
